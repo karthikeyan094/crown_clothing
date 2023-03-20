1349983bef7d184b2d7a26eab93c78d6fa4390f4
karthikeyan094 <karthikeyan094@github.com>
2023-03-20 12:18:28 +0530

    created directory category item components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import './categories-style.scss'
-import  CategoryItem from './component/category-item/category-item.component';
+import Directory from "./component/directory/directory.component";
 const App = () => {
   const categories = [
   {
@@ -30,11 +29,7 @@ const App = () => {
 ]
 
   return (
-    <div className="categories-container">
-      {categories.map(( category) => (
-        <CategoryItem key={category.id} category={category} />
-    ))}
-    </div>
+   <Directory categories={categories}/>
   );
 }
 
