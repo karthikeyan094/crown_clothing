61ce524a463144083ac74c9f378cf269f00a4f08
karthikeyan094 <karthikeyan094@github.com>
2023-04-12 21:41:33 +0530

    added sign-in component and firebase utils

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,8 +1,16 @@
+import { signInWithGooglePopUp, createUserDocumentWithAuth } from "../../utils/firebase/firebase.utils";
 const SignIn = () => {
+    const logGoogleUser = async () => {
+        const { user } = await signInWithGooglePopUp();
+        console.log(user);
+        const userDocRef = await createUserDocumentWithAuth(user);
+
+    }
     return (
         <>
             <div>
                 <h1> Sign In page</h1>
+                <button onClick={logGoogleUser}>Sign in with google</button>
             </div>
         </>
     )
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import {getAuth,signInWithPopup, signInWithRedirect,GoogleAuthProvider} from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBLCTRwRwbmFCwtboB1bQSeb5WtApaBymw",
@@ -17,5 +18,24 @@ provider.getCustomParameters({
 });
 
 export const auth = getAuth();
-export const signInWithGooglePopUp = () => signInWithPopup(auth,provider);
+export const signInWithGooglePopUp = () => signInWithPopup(auth, provider);
+export const db = getFirestore();
+
+export const createUserDocumentWithAuth = async (userAuth) => {
+    const userDocRef = doc(db, 'users', userAuth.uid);
+    const userSnapshot = await getDoc(userDocRef);
+    console.log(userSnapshot);
+    console.log(userSnapshot.exists());
+    //if user doesn't exist
+    if (!userSnapshot.exists()) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+        try {
+            await setDoc(userDocRef, { displayName, email, createdAt });
+        } catch (error) {
+            console.log("error creating the user", error.message);            
+        }
+    }
+    return userDocRef;
+}
 
